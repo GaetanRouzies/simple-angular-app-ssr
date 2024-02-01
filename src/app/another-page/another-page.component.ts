a3cc96afbcf1d95875a03732f245db7f5a112f05
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-02-01 06:03:25 +0100

    Initial commit

---
+++ src/app/another-page/another-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-another-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './another-page.component.html',
+  styleUrl: './another-page.component.scss'
+})
+export class AnotherPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AnotherPageComponent} from "./another-page/another-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'another-page', component: AnotherPageComponent },
+];
