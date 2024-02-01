b1e071bf03f7ad984bf799d030c0b8d476837297
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-02-01 06:30:06 +0100

    Update base ref and prerendered routes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VGD6ED5T.mjs";import"./chunk-SBQGHV3E.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-NU2ZEUOY.mjs";import"./chunk-B5NWGNLA.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
